38f68bdc1b652ee662af893fde41bd25e22d09f7
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2025-11-03 10:14:32 -0300

    lucas - fix de fornteira, walkers não podem sair da matrix

--- random_walk.m
+++ random_walk.m
@@ -29,10 +29,41 @@ while numpassos < limite
     if sentido == 0
       sentido = -1;
     endif
+    
+    %{
     if dir == 0 && M(X(ii) + sentido, Y(ii)) == 0
       X(ii) += sentido;
     elseif dir == 1 && M(X(ii), Y(ii) + sentido) == 0
       Y(ii) += sentido;
+    endif
+    %}
+
+    if dir == 0
+
+      % andando para direita, tem que estar dentro da matriz e livre
+      if sentido == 1 && X(ii) + sentido <= n_matriz && M(X(ii) + sentido, Y(ii)) == 0
+        X(ii) += sentido;
+      endif
+      
+      % andando para esquerda
+      if sentido == -1 && X(ii) + sentido >= 1 && M(X(ii) + sentido, Y(ii)) == 0
+        X(ii) += sentido;
+      endif
+
+    endif
+
+    if dir == 1
+    
+      % andando para baixo, tem que estar dentro da matriz e livre
+      if sentido == 1 && Y(ii) + sentido <= n_matriz && M(X(ii), Y(ii) + sentido) == 0
+        Y(ii) += sentido;
+      endif
+      
+      % andando para cima
+      if sentido == -1 && Y(ii) + sentido >= 1 && M(X(ii), Y(ii) + sentido) == 0
+        Y(ii) += sentido;
+      endif
+
     endif
 
     if mod(numpassos, passo_medicao) == 0

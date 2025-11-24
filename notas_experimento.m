cfb7014d6ab5b335689060bfcd7f53ae14f88b8c
andreasmzz <ag_muzzi@id.uff.br>
2025-11-24 06:47:30 -0300

    novas execucoes de teste para iterativo

--- notas_experimento.m
+++ notas_experimento.m
@@ -22,6 +22,21 @@ tempo = 238 segundos -> 4 minutos
 
 
 
+1 ver = resultados_experimento.csv
+  iter e vat não usm mesmos parâmetros
+
+2 ver = resultado_experimento.csv (do git, aqui deu ruim)
+  só vet
+
+3 ver = resultado_experimento1.csv
+  iter muito pequeno
+
+4 ver = resultado_experimento_iter.csv
+  só iter com os mesmo parâmetros de vet mas só até 1 Mi ao invés de 1 Bi
+
+
+
+
 
 
 #}
